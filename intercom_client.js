0e93d7fae7dde0150912813015cec127d837e011
Mikael Lirbank <mikael@lirbank.com>
2014-05-29 16:49:52 -0700

    Remove trailing whitespace

--- intercom_client.js
+++ intercom_client.js
@@ -3,16 +3,16 @@ Meteor.subscribe('intercomHash');
 var minimumUserInfo = function(user) {
   var info = {
     app_id: Meteor.settings.public.intercom.id,
-    
+
     user_id: user._id,
-    
+
     created_at: Math.round(Meteor.user().createdAt/1000)
   }
-  
+
   // they actually need to have this but it can be useful for testing
   if (user.intercomHash)
     info.user_hash = user.intercomHash;
-  
+
   return info;
 }
 
@@ -31,21 +31,21 @@ Meteor.startup(function() {
     var user = Meteor.user();
     if (!user) // "log out"
       return Intercom('shutdown');
-  
+
     var info = IntercomSettings.minimumUserInfo(user);
     if (IntercomSettings.userInfo) {
       var ready = IntercomSettings.userInfo(user, info);
       if (ready === false)
         return;
     }
-      
-    
+
+
     if (info) {
       var type = booted ? 'update': 'boot';
-      
+
       // console.log(type, info)
       Intercom(type, info);
-      
+
       booted = true;
     }
   });
--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.on_use(function (api, where) {
   api.use('session', 'client');
   api.add_files('intercom_server.js', 'server');
   api.add_files(['intercom_loader.js', 'intercom_client.js'], 'client');
-  
+
   api.export('IntercomSettings');
   api.export('IntercomHash', 'server');
 });
